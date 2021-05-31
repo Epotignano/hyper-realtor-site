86e723ba142d5e12f22930ddd131d9a0b6c69669
epotignano <me@epotignano.com>
2021-05-30 22:43:17 -0500

    Purging made well

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,25 +6,9 @@ module.exports = {
   //     purgeLayersByDefault: true,
   //     removeDeprecatedGapUtilities: true,
   //   },
-  plugins: [
-    // for tailwind UI users only
-    require('@tailwindcss/ui'),
-    // other plugins here
-  ],
-  purge: {
-    content: [
+  purge:  [
       "./src/**/*.svelte",
       // may also want to include HTML files
       "./src/**/*.html"
     ],
-    // this is for extracting Svelte `class:` syntax but is not perfect yet, see below
-    defaultExtractor: content => {
-      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-      const broadMatchesWithoutTrailingSlash = broadMatches.map(match => _.trimEnd(match, '\\'))
-      const matches = broadMatches
-        .concat(broadMatchesWithoutTrailingSlash)
-      return matches
-    },
-    enabled: !isProduction // disable purge in dev
-  },
 };
